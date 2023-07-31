2d5b60ba104d548ac1057dcd3946878a840cad38
inferno268 <marek.macku@seznam.cz>
2023-07-31 15:47:21 +0200

    Register on JSON and password Encoder

--- src/main/java/com/ruthless/lp/Config/UserDTOMapper.java
+++ src/main/java/com/ruthless/lp/Config/UserDTOMapper.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UserDTOConfig {
+public class UserDTOMapper {
 
     @Bean
     public ModelMapper modelMapper() {
